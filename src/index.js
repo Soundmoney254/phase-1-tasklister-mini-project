ab8918a4bbe06c385e12a03d1562821f8f7d32d4
Samuel <samnmbuguah@gmail.com>
2023-03-24 08:09:55 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    let form = document.querySelector("form")
+    form.addEventListener("submit", event => {
+      event.preventDefault()
+      createNewTask(event.target.newTask.value,event.target.dueDate.value);
+      form.reset();
+    })
+  })
+  function createNewTask(task,date){
+    let list = document.createElement("li");
+    let deleteButton =  document.createElement("button");
+    deleteButton.addEventListener("click", handleDelete)
+    deleteButton.textContent = " X ";
+    list.textContent = ` ${task}  by  ${date}  `;
+    list.appendChild(deleteButton);
+    tasks.appendChild(list);
+  }
+  function handleDelete(event){
+    event.target.parentNode.remove();
+  }
